0c8bfcd0057b00dbd7c5117481eb0c63fa78939a
Bae Yujin <ujini7833@gmail.com>
2025-12-10 00:44:16 +0900

    Feat/#2: 지난 여행 관리

--- src/screens/HomeScreen/TripList.js
+++ src/screens/HomeScreen/TripList.js
@@ -1,43 +1,61 @@
 export const upcomingTrips = [
   {
+    id: 1,
     title: '일본 오사카',
     dDay: 3,
     startDate: '2026.09.03',
     endDate: '2026.09.05',
+    location: 'A',
+    companions: ['1', '2'],
     color: '#3C74D4',
   },
   {
+    id: 2,
     title: '강릉',
     dDay: 26,
     startDate: '2026.09.26',
     endDate: '2026.09.27',
+    location: 'B',
+    companions: ['3', '4'],
     color: '#FFE386',
   },
 ];
 
 export const pastTrips = [
   {
+    id: 1,
     title: '제주 여행',
     startDate: '2025.06.01',
     endDate: '2025.06.03',
+    location: 'C',
+    companions: ['5', '6'],
     color: '#EAAF4F',
   },
   {
-    title: '성심당 가고싶당',
+    id: 2,
+    title: '성심당 가고심당',
     startDate: '2025.01.22',
     endDate: '2025.01.24',
+    location: 'D',
+    companions: ['7', '8'],
     color: '#B8CDFF',
   },
   {
+    id: 3,
     title: '속초로 떠나자!',
     startDate: '2025.11.14',
     endDate: '2025.11.15',
+    location: 'E',
+    companions: ['9', '10'],
     color: '#A4C664',
   },
   {
+    id: 4,
     title: '일본 여행',
     startDate: '2025.07.04',
     endDate: '2025.07.07',
+    location: 'F',
+    companions: ['11', '12'],
     color: '#EE8787',
   },
 ];
